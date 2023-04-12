db133625202440fba13f61b2b94b42d572ed8529
krywa5 <blocker92@o2.pl>
2023-04-12 20:50:55 +0200

    Updated cors proxy

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,65 +1,66 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 
-import CustomBackend from './CustomBackend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import CustomBackend from "./CustomBackend";
+import LanguageDetector from "i18next-browser-languagedetector";
 
+const corsProxy = "https://corsproxy.io/?";
 
 i18n
-    .use(CustomBackend)
-    .use(LanguageDetector)
-    .use(initReactI18next)
-    .init({
-        defaultLanguage: 'en',
-        otherLanguages: ['pl'],
-        fallbackLng: 'en',
-        debug: true,
-        saveMissing: true,
+  .use(CustomBackend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    defaultLanguage: "en",
+    otherLanguages: ["pl"],
+    fallbackLng: "en",
+    debug: true,
+    saveMissing: true,
 
-        backend: {
-            // cors-anywhere is a trick. Don't use it in production
-            loadPath: 'https://cors-anywhere.herokuapp.com/https://api.poeditor.com/v2/terms/list',
-            addPath: 'https://cors-anywhere.herokuapp.com/https://api.poeditor.com/v2/terms/add',
-            crossDomain: true,
-            parse: data => {
-                const parsedData = JSON.parse(data);
-                const terms = parsedData.result.terms.reduce((acc, item) => {
-                    acc[item.term] = item.translation.content || item.term;
+    backend: {
+      loadPath: `${corsProxy}https://api.poeditor.com/v2/terms/list`,
+      addPath: `${corsProxy}https://api.poeditor.com/v2/terms/add`,
+      crossDomain: true,
+      parse: (data) => {
+        const parsedData = JSON.parse(data);
+        const terms = parsedData.result.terms.reduce((acc, item) => {
+          acc[item.term] = item.translation.content || item.term;
 
-                    return acc;
-                }, {});
+          return acc;
+        }, {});
 
-                return terms;
-            },
-            parsePayload: (namespace, key) => {
-                if (key === '_t') return;
+        return terms;
+      },
+      parsePayload: (namespace, key) => {
+        if (key === "_t") return;
 
-                const data = [{
-                    term: key,
-                }];
-                const payload = {
-                    api_token: process.env.REACT_APP_POEDITOR_TOKEN,
-                    data: JSON.stringify(data),
-                    id: process.env.REACT_APP_POEDITOR_APP_ID,
-                };
+        const data = [
+          {
+            term: key,
+          },
+        ];
+        const payload = {
+          api_token: process.env.REACT_APP_POEDITOR_TOKEN,
+          data: JSON.stringify(data),
+          id: process.env.REACT_APP_POEDITOR_APP_ID,
+        };
 
-                return payload;
-            },
-            parseLoadPayload: ({ lng }) => {
-                const payload = {
-                    api_token: process.env.REACT_APP_POEDITOR_TOKEN,
-                    language: lng,
-                    id: process.env.REACT_APP_POEDITOR_APP_ID,
-                };
+        return payload;
+      },
+      parseLoadPayload: ({ lng }) => {
+        const payload = {
+          api_token: process.env.REACT_APP_POEDITOR_TOKEN,
+          language: lng,
+          id: process.env.REACT_APP_POEDITOR_APP_ID,
+        };
 
-                return payload;
-            },
-        },
+        return payload;
+      },
+    },
 
-        interpolation: {
-            escapeValue: false, // not needed for react as it escapes by default
-        },
-    });
-
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+  });
 
 export default i18n;
